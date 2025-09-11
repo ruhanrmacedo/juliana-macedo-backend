fa8bb997ef4cb6d47722184ac527f806237642b9
Ruhan <ruhan_350@hotmail.com>
2025-09-10 23:07:16 -0300

    Atualização

--- src/models/anthropometry/calculators/utils/number.ts
+++ src/models/anthropometry/calculators/utils/number.ts
@@ -1,10 +1,12 @@
+import { BadRequest } from "./errors";
+
 export function normalizeNumber(v: unknown): number {
     if (typeof v === "number") return v;
     if (typeof v === "string") {
         const s = v.replace(/\s+/g, "").replace(",", ".");
         const n = Number(s);
-        if (Number.isNaN(n)) throw new Error(`Valor numérico inválido: ${v}`);
+        if (Number.isNaN(n)) throw new BadRequest(`Valor numérico inválido: ${v}`); // <- aqui
         return n;
     }
-    throw new Error(`Tipo inválido para número: ${typeof v}`);
+    throw new BadRequest(`Tipo inválido para número: ${typeof v}`); // <- aqui
 }
